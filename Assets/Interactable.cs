5211ae5c32228fa7de0166a93fc42d61b35d326f
mathiasTanner <mathias.tanner.ge@gmail.com>
2023-02-04 16:16:45 +0100

    debug written

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -15,7 +15,7 @@ public class Interactable : MonoBehaviour
 
     public virtual void Interact(GameObject playerGameObject)
     {
-        Debug.Log("In Interact");
+
     }
 
     void OnTriggerEnter2D(Collider2D collision)
